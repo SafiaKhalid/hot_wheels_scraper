a0fd38d7f57c7357d5ff62be7fc2bd510f199962
SafiaKhalid <safia.am.khalid@gmail.com>
2024-08-20 16:19:51 +0100

    Removed thead selector in headingsRow const in getData,js

---
+++ dist/getData.dev.js
@@ -0,0 +1,81 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _puppeteer = _interopRequireDefault(require("puppeteer"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var getData = function getData() {
+  var browser, page, tables;
+  return regeneratorRuntime.async(function getData$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(_puppeteer["default"].launch({
+            headless: false,
+            defaultViewport: null
+          }));
+
+        case 2:
+          browser = _context.sent;
+          _context.next = 5;
+          return regeneratorRuntime.awrap(browser.newPage());
+
+        case 5:
+          page = _context.sent;
+          _context.next = 8;
+          return regeneratorRuntime.awrap(page["goto"]('https://hotwheels.fandom.com/wiki/List_of_1978_Hot_Wheels', {
+            waitUntil: 'domcontentloaded'
+          }));
+
+        case 8:
+          _context.next = 10;
+          return regeneratorRuntime.awrap(page.evaluate(function () {
+            var table = document.querySelector('table.wikitable');
+            var headingsRow = table.querySelectorAll('tr > th');
+            var headingsArray = [];
+            headingsRow.forEach(function (heading) {
+              headingsArray.push(heading.innerText);
+            });
+            var rows = table.querySelectorAll('tbody > tr:not(:first-child)');
+            var year_models = [];
+            rows.forEach(function (row) {
+              var object = {
+                /* [headingsArray[0]]: row.querySelectorAll('td')[0].innerText,
+                [headingsArray[1]]: row.querySelectorAll('td')[1].innerText,
+                [headingsArray[2]]: row.querySelectorAll('td')[2].innerText,
+                [headingsArray[3]]: row.querySelectorAll('td')[3].innerText,
+                [headingsArray[4]]: row.querySelectorAll('td')[4].innerText, */
+              };
+              headingsArray.forEach(function (heading, index) {
+                object[heading] = row.querySelectorAll('td')[index].innerText;
+              });
+              year_models.push(object);
+            });
+            return {
+              headingsArray: headingsArray,
+              year_models: year_models
+            };
+          }));
+
+        case 10:
+          tables = _context.sent;
+          console.log(tables);
+          _context.next = 14;
+          return regeneratorRuntime.awrap(browser.close());
+
+        case 14:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
+
+var _default = getData;
+exports["default"] = _default;
--- getData.js
+++ getData.js
@@ -8,13 +8,13 @@ const getData = async () => {
 
   const page = await browser.newPage();
 
-  await page.goto('https://hotwheels.fandom.com/wiki/List_of_2022_Hot_Wheels', {
+  await page.goto('https://hotwheels.fandom.com/wiki/List_of_1978_Hot_Wheels', {
     waitUntil: 'domcontentloaded',
   });
 
   const tables = await page.evaluate(() => {
     const table = document.querySelector('table.wikitable');
-    const headingsRow = table.querySelectorAll('tbody > tr > th');
+    const headingsRow = table.querySelectorAll('tr > th');
     let headingsArray = [];
 
     headingsRow.forEach((heading) => {
@@ -26,13 +26,17 @@ const getData = async () => {
     let year_models = [];
 
     rows.forEach((row) => {
-      const object = {
-        [headingsArray[0]]: row.querySelectorAll('td')[0].innerText,
+      let object = {
+        /* [headingsArray[0]]: row.querySelectorAll('td')[0].innerText,
         [headingsArray[1]]: row.querySelectorAll('td')[1].innerText,
         [headingsArray[2]]: row.querySelectorAll('td')[2].innerText,
         [headingsArray[3]]: row.querySelectorAll('td')[3].innerText,
-        [headingsArray[4]]: row.querySelectorAll('td')[4].innerText,
+        [headingsArray[4]]: row.querySelectorAll('td')[4].innerText, */
       };
+      headingsArray.forEach((heading, index) => {
+        object[heading] = row.querySelectorAll('td')[index].innerText;
+      });
+
       year_models.push(object);
     });
 

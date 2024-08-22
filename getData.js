385bfceb95151808515e6c3a00377052e69401c5
SafiaKhalid <safia.am.khalid@gmail.com>
2024-08-22 16:40:44 +0100

    getData() returns nested array of headings of each table in selected wiki page, and nested array of content of each table in selected wiki page

--- getData.js
+++ getData.js
@@ -1,51 +1,51 @@
 import puppeteer from 'puppeteer';
 
 const getData = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-
-  const page = await browser.newPage();
-
-  await page.goto('https://hotwheels.fandom.com/wiki/List_of_1978_Hot_Wheels', {
-    waitUntil: 'domcontentloaded',
-  });
-
-  const tables = await page.evaluate(() => {
-    const table = document.querySelector('table.wikitable');
-    const headingsRow = table.querySelectorAll('tr > th');
-    let headingsArray = [];
-
-    headingsRow.forEach((heading) => {
-      headingsArray.push(heading.innerText);
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
     });
 
-    const rows = table.querySelectorAll('tbody > tr:not(:first-child)');
+    const page = await browser.newPage();
 
-    let year_models = [];
+    await page.goto(
+        'https://hotwheels.fandom.com/wiki/List_of_1978_Hot_Wheels',
+        {
+            waitUntil: 'domcontentloaded',
+        }
+    );
 
-    rows.forEach((row) => {
-      let object = {
-        /* [headingsArray[0]]: row.querySelectorAll('td')[0].innerText,
-        [headingsArray[1]]: row.querySelectorAll('td')[1].innerText,
-        [headingsArray[2]]: row.querySelectorAll('td')[2].innerText,
-        [headingsArray[3]]: row.querySelectorAll('td')[3].innerText,
-        [headingsArray[4]]: row.querySelectorAll('td')[4].innerText, */
-      };
-      headingsArray.forEach((heading, index) => {
-        object[heading] = row.querySelectorAll('td')[index].innerText;
-      });
+    const tables = await page.evaluate(() => {
+        const tables = document.querySelectorAll('table.jquery-tablesorter');
 
-      year_models.push(object);
+        let headingRows = [];
+        let rowsArrays = [];
+
+        tables.forEach((table) => {
+            headings = table.querySelectorAll('tr > th');
+            rows = table.querySelectorAll('tbody > tr');
+
+            let headingNested = [];
+            headings.forEach((heading) => {
+                headingNested.push(heading.innerText);
+            });
+            headingRows.push(headingNested);
+
+            let rowsNested = [];
+            rows.forEach((row) => {
+                rowsNested.push(row.innerText);
+            });
+            rowsArrays.push(rowsNested);
+        });
+
+        let year_models = [];
+
+        return { headingRows, rowsArrays, year_models };
     });
 
-    return { headingsArray, year_models };
-  });
+    console.log(tables);
 
-  console.log(tables);
-
-  await browser.close();
+    await browser.close();
 };
 
 export default getData;

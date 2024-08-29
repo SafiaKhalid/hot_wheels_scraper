1a7efafc1954b985e047768366ff6ac0055f968c
SafiaKhalid <safia.am.khalid@gmail.com>
2024-08-29 14:21:49 +0100

    Added generateBook.js

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import getData from './getData.js';
 
 let wikiData = [];
 
-for (let i = 1974; i <= 2025; i++) {
+/* for (let i = 1974; i <= 2025; i++) {
+    let object = {};
+    let data = await getData(i);
+    object[i] = data;
+
+    wikiData.push(object);
+} */
+
+for (let i = 1974; i <= 1976; i++) {
     let object = {};
     let data = await getData(i);
     object[i] = data;
@@ -10,4 +18,7 @@ for (let i = 1974; i <= 2025; i++) {
     wikiData.push(object);
 }
 
-console.log(wikiData);
+console.log(JSON.stringify(wikiData));
+
+//Save data to spreadsheet (excel?) with each year in different worksheet
+//exceljs?

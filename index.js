e70bd41bfeb970d964a2eb9835ebbf31c3f8b1ff
SafiaKhalid <119669832+SafiaKhalid@users.noreply.github.com>
2024-09-10 12:18:26 +0100

    Changed selector in getData.js to select tr in tbody if th elements exist

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ import generateBook from './generateBook.js';
 
 let wikiData = [];
 
-/* for (let i = 1974; i <= 2025; i++) {
-    let object = {};
-    let data = await getData(i);
-    object[i] = data;
-
-    wikiData.push(object);
-} */
-
-for (let i = 2000; i <= 2025; i++) {
+for (let i = 2010; i <= 2015; i++) {
     let object = {};
     let data = await getData(i);
     object[i] = data;
